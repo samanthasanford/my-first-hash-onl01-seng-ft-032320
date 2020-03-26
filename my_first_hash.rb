638f23d0763df95682843e4f1d77a39ebbb72d1c
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 22:50:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,7 +42,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   
-  shipping_manifest["muskests"] = 2 
+  shipping_manifest["muskets"] = 2 
 
 
   # add 4 gun powder to the shipping_manifest hash below
