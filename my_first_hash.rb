6951152862a9a5f1a741cac78408f4ff74bf73d7
Samantha Sanford <sanfordsamantha@icloud.com>
2020-03-26 22:44:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,11 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+
   my_hash = {
-    "name" => "Samantha"
-    "age" => 27
-    "Location" => "Nevada"
+    "name" => "Samantha",
+    "age" => 27,
+    "Location" => "Nevada",
   }
 
-end
 end
 
 
@@ -14,8 +13,13 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3,
+  }
 
-
+  return the_manifest
 end
 
 def retrieval
@@ -26,6 +30,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  
+  
 
 end
 
